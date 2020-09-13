47b1e92c331a6341ab70ed2adf4ebf9a7426ff24
Avraam Marimpis <makhsm@gmail.com>
2020-09-13 14:02:04 +0200

    Add sample evaluating JSON data from a Schema.

--- endpoint_api.go
+++ endpoint_api.go
@@ -1,22 +1,56 @@
 package main
 
 import (
+	"context"
 	"encoding/json"
-	//"github.com/qri-io/jsonschema"
+	"fmt"
+	"github.com/qri-io/jsonschema"
+	"io"
 	"log"
 	"net/http"
+	"os"
 	"time"
-	"fmt"
 )
 
 type APIResponse struct {
 	Message string  `json:"message"`
 }
 
-func pingApi() {
+func PingApi() {
 	url := "http://0.0.0.0:5500/v1/api/ping/"
+	fmt.Println("Probing", url)
 
-	fmt.Println(url)
+	ctx := context.Background()
+	var schemaData = []byte(`{
+		"$id": "https://qri.io/schema/",
+		"$comment" : "",
+		"title": "APIResponse",
+		"type": "object",
+		"properties": {
+			"message": {
+				"type": "string"
+			}
+		},
+		"required": ["message"]
+	}`)
+
+	rs := &jsonschema.Schema{}
+	if err := json.Unmarshal(schemaData, rs); err != nil {
+		panic("unmarshal schema: " + err.Error())
+	}
+
+	var valid = []byte(`{
+	    "message" : "pong"
+    }`)
+
+	errs, err := rs.ValidateBytes(ctx, valid)
+	if err != nil {
+		panic(err)
+	}
+
+	if len(errs) > 0 {
+		fmt.Println(errs[0].Error())
+	}
 
 	client := http.Client{Timeout: time.Second * 5}
 	request, err := http.NewRequest("GET", url, nil)
@@ -29,6 +63,8 @@ func pingApi() {
 		log.Fatalln(err)
 	}
 	defer resp.Body.Close()
+
+	io.Copy(os.Stdout, resp.Body)
 
 	responseMessage := APIResponse{}
 	json.NewDecoder(resp.Body).Decode(&responseMessage)
